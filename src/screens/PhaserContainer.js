a6965e830a03b0b6833032964cdcd3307baca767
Disco Lelis <rodolfo.lelis@gmail.com>
2024-05-09 18:35:44 -0300

    event handlers for key down added, inproving the timing mechanic

--- src/screens/PhaserContainer.js
+++ src/screens/PhaserContainer.js
@@ -50,6 +50,7 @@ const PhaserContainer = () => {
     // 4/4 beat state
     this.beatIndex = 0;
     this.beatsPerMeasure = 4;
+    this.hitOnFourth = false;
 
     // Play dance audio track
     this.danceTrack = this.sound.add('danceTrack');
@@ -58,6 +59,22 @@ const PhaserContainer = () => {
     // Rectangle for flashing effect
     beatRectangle = this.add.rectangle(700, 50, 100, 100, 0xff0000);
     beatRectangle.setAlpha(0);
+
+    // Event listeners for key presses
+    this.input.keyboard.on('keydown-A', (event) => handleKeydown('A'), this);
+    this.input.keyboard.on('keydown-S', (event) => handleKeydown('S'), this);
+    this.input.keyboard.on('keydown-Z', (event) => handleKeydown('Z'), this);
+    this.input.keyboard.on('keydown-X', (event) => handleKeydown('X'), this);
+
+    const handleKeydown = (key) => {
+      // Only consider the key press if it's the 4th beat
+      if (this.hitOnFourth) {
+        console.log(`${key} key pressed exactly on the 4th beat!`);
+        this.score += 1000;
+        scoreText.setText('Score: ' + this.score);
+        this.hitOnFourth = false; // Reset the boolean after a correct hit
+      }
+    };
   
     const updateBeat = function () {
       // Update to the next beat
@@ -65,6 +82,10 @@ const PhaserContainer = () => {
   
       // Flash red at beats 1-3, green on the 4th beat
       if (beatIndex === 3) {
+        this.hitOnFourth = true;
+        setTimeout(() => {
+          this.hitOnFourth = false; // Reset after 200x  ms to enforce exact timing
+        }, 200);
         beatRectangle.setFillStyle(0x00ff00); // Green on the 4th beat
         
       } else {
@@ -120,12 +141,13 @@ const PhaserContainer = () => {
 
   const update = function () {
     // Check if it's the 4th beat
-    if (beatIndex === 3) {
+    /*if (beatIndex === 3 && this.hitOnFourth) {
       // Check if any of the action keys are pressed
-      if (actionKeys.A.isDown) {
+      if (actionKeys.A.isPressed) {
         console.log("A key pressed on the 4th beat!");
         this.score += 1000;
         this.rhythmScore += 1000;
+        this.hitOnFourth = false;
         scoreText.setText(
           'Total Score: ' + this.score + '\nRhythm Score: ' + this.rhythmScore
         );
@@ -142,7 +164,7 @@ const PhaserContainer = () => {
       if (actionKeys.X.isDown) {
         console.log("X key pressed on the 4th beat!");
       }
-    }
+    }*/
   };
 
   return (
